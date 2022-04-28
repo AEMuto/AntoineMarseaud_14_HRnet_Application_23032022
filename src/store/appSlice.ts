bb870a6235ded2817a8df3a365ecd71fede08ce9
AEMuto <antoine.marseaud@gmail.com>
2022-04-28 19:58:20 +0200

    Defining basic types for the employees data.

---
+++ src/store/appSlice.ts
@@ -0,0 +1,13 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+
+const initialState = {
+	isLoading: false,
+
+}
+
+export const appSlice = createSlice({
+	name:'app',
+	initialState,
+	reducers: {}
+})
